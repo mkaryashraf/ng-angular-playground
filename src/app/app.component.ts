939cd37a43a81b59c828de0e0b908a9dc170c155
unknown <mkaryashraf@gmail.com>
2019-09-23 18:25:48 +0200

    property binding and templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  /*template:`
+  <h1>hey there</h1>
+  <p>I am fine {{myObj.name}}</p>
+  `,*/
+  template:
+  `<h1>hey there</h1>
+  <img src = {{angularLogo}}>
+  <img [src] = "angularLogo">
+  <img bind-src ="angularLogo">
+  `
+  /*`
+  <h1>hey there</h1>
+ <ul>
+     <li *ngIf="!myArr;else myothertemp">yeah I exist!</li>
+  </ul>
+  <ng-template #myothertemp> hopa lala</ng-template>
+  `*/,
+  //templateUrl:'./app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-angular-playground';
+//  title = 'ng-angular-playground';
+//myArr= 'nothing';
+angularLogo="https://angular.io/resources/images/logos/angular2/angular.png";
 }
